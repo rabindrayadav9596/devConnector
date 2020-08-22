4b0f7fa41476be737b24c6e10428b81e6cce7466
Rabindra Yadav <rabindrayadav9596@gmail.com>
2020-08-21 23:13:59 -0500

    upload

--- README.md
+++
@@ -1,3 +0,0 @@
-# devConnector
-
-This is the tutorial follow project of udemy in which I am going to build clone for social network website.
--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://rabin:rabin@cluster0.y3kag.mongodb.net/devconnector?retryWrites=true&w=majority",
-};
